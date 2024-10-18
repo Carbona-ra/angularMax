f94f75fd047728d33f36f67e11d4e2d8102ef076
Benjamin Schacher <benjamin.schacher@laposte.net>
2024-10-18 12:31:31 +0200

    azeazeazeazeazeza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./other/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angularMax';
+export class AppComponent implements OnInit {
+  
+
+  /*
+  * 
+  * VARIABLE GLOBAL
+  * 
+  * */
+
+  APP_ENV: string = "DEV"; /* PROD or DEV */
+  API_URL: string = "";
+  // TOKEN_API
+
+  ngOnInit(): void {
+
+
+
+
+    /* GESTION DE L'API */
+    if (this.APP_ENV == "DEV") {
+      this.API_URL = "http://localhost:8000/api";
+    } else if( this.APP_ENV == "PROD") {
+      this.API_URL = "http://localhost:8000/api";
+    } else {
+      this.API_URL = "http://localhost:8000/api";
+    }
+  }
+
+
+  /* 
+  *   
+  * FONCTION GLOBAL
+  * 
+  * */
+
+  creatCors(): void {
+
+  }
+
+ /* 
+  *   
+  * FONCTION GLOBAL
+  * 
+  * */
+
+
+
+
 }
